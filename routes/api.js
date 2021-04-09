0221c3b9566ab9cfd619c24e547d454cfcee6429
Nitin Mukesh <nitin.mukesh@sine.co>
2021-04-09 17:04:43 +0930

    Fixed callback route

--- routes/api.js
+++ routes/api.js
@@ -35,8 +35,10 @@ router.get("/api/transaction", (req, res) => {
 });
 
 router.post("/api/callbacks/:credentialMappingId", (req, res) => {
-  console.log(JSON.stringify(req.body));
-  res.status(200);
+  console.log("req.body", JSON.stringify(req.body));
+  console.log("req.query", JSON.stringify(req.query));
+  console.log("req.params", JSON.stringify(req.params));
+  res.status(200).json();
 });
 
 module.exports = router;
