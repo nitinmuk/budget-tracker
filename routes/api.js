23b319de31e09dd0a3db9f2e206d9469473f88c6
Nitin Mukesh <nitin.mukesh@sine.co>
2021-04-12 10:26:54 +0930

    Improved callbacklogging

--- routes/api.js
+++ routes/api.js
@@ -35,9 +35,9 @@ router.get("/api/transaction", (req, res) => {
 });
 
 router.post("/api/callbacks/:credentialMappingId", (req, res) => {
-  console.log("req.body", JSON.stringify(req.body));
-  console.log("req.query", JSON.stringify(req.query));
-  console.log("req.params", JSON.stringify(req.params));
+  console.log("------req.body--------\n", JSON.stringify(req.body));
+  console.log("--------req.query------\n", JSON.stringify(req.query));
+  console.log("--------req.params-------\n", JSON.stringify(req.params));
   res.status(200).json();
 });
 
